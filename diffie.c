005ee955d9c3bde7b7681d6a2b459920bfd86d5c
how2run <85328640+how2run@users.noreply.github.com>
2023-05-25 10:49:37 +0530

    Update diffie.c

--- diffie.c
+++ diffie.c
@@ -1,27 +1,37 @@
-#include<stdio.h>
-int compute(int a,int m,int n)
+#include <stdio.h>
+int compute(int a, int m, int n)
 {
     int r;
-    int y=1;
-    while(m>0)
+    int y = 1;
+    while (m > 0)
     {
-        r=m%2;
-        if(r==1){
-            y=(y*a)%n;
+        r = m % 2;
+        if (r == 1) {
+            y = (y*a) % n;
         }
-        a=(a*a)%n;
-        m=n/2;
+        a = a*a % n;
+        m = m / 2;
     }
     return y;
 }
-int p=23;
-int g=5;
-int a,b;
-int A,B;
-int a=6;
-A=compute(g,a,p);
-b=15;
-B=compute(g,b,p);
-int A=compute(B,a,p);
-int B=compute(A,b,p);
-printf("alice secret key is %d %d",A,B);
+int main()
+{
+    int p,g,A,B;            
+    int Xa, Xb;      
+    printf("Enter the common prime no.: ");
+    scanf("%d",&p);
+    printf("Enter the primitive root of %d : ",p);
+    scanf("%d",&g);
+    printf("Enter the private key of A 'Xa':  ");
+    scanf("%d",&Xa);      
+    A = compute(g, Xa, p);
+    printf("Enter the private key of B 'Xb':  ");
+    scanf("%d",&Xb);       
+    B = compute(g, Xb, p);
+    int keyA = compute(B, Xa, p);
+    int keyB = compute(A, Xb, p);
+    printf("Public Key of A:%d\n",A);
+    printf("Public Key of B:%d\n",B);
+    printf("Alice's secret key is %d\nBob's secret key is %d\n", keyA, keyB);
+    return 0;
+}
